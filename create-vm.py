9984708b471f0f0d1f761005070935a09fb6b0c0
Jacob Castello <jacob@arin.net>
2015-11-05 16:26:44 -0500

    updating to use json config file

--- create-vm.py
+++ create-vm.py
@@ -21,7 +21,8 @@ import getopt
 import optparse
 import os
 import time
-
+import json
+import getpass
 
 from ovirtsdk.api import API
 from ovirtsdk.xml import params
@@ -50,12 +51,61 @@ p.add_option("--sdtype", dest="sdtype", help="SD type", metavar="sdtype", defaul
 p.add_option("--sdsize", dest="sdsize", help="SD size in GB", metavar="sdsize", default="20")
 p.add_option("--osver", dest="osver", help="OS version", metavar="osver", default="rhel_6x64")
 p.add_option("--vmnet", dest="vmnet", help="Network to use", metavar="vmnet", default="rhevm")
-
+p.add_option("--config", dest="config", help="JSON config file")
+p.add_option("--storage", dest="storage_name", help="Name of the storage domain")
+p.add_option("--ca", dest="ca_file", help="Path to the ca file")
+p.add_option("--insecure", dest="insecure", help="Connect without validating the CA")
 (options, args) = p.parse_args()
 
-baseurl = "https://%s" % (options.server)
+# Use the json file to update configs if present
+if options.config:
+    with open(options.config) as config_params:
+        locals().update(json.load(config_params))
 
-api = API(url=baseurl, username=options.username, password=options.password, insecure=True)
+# Overide anything in the config file
+# on the command line
+if options.username:
+    username = options.username
+if options.server:
+    server = options.server
+if options.name:
+    name = options.name
+if options.cluster:
+    cluster = options.cluster
+if options.vmcpu:
+    vmcpu = options.vmcpu
+if options.vmem:
+    vmem = options.vmem
+if options.sdtype:
+    sdtype = options.sdtype
+if options.sdsize:
+    sdsize = options.sdsize
+if options.osver:
+    osver = options.osver
+if options.vmnet:
+    vmnet = options.vmnet
+if options.verbosity:
+    verbosity = options.verbosity
+if options.storage_name:
+    storage_name = options.storage_name
+if options.ca_file:
+    ca_file = options.ca_file
+if options.insecure:
+    insecure = options.insecure
+
+# Allow for interactive password auth
+if options.password:
+    password = options.password
+else:
+    password = getpass.getpass()
+
+
+baseurl = "https://%s" % (server)
+
+if options.insecure:
+    api = API(url=baseurl, username=username, password=password, insecure=True)
+else:
+    api = API(url=baseurl, username=username, password=password, ca_file=ca_file)
 
 try:
     value = api.hosts.list()
@@ -63,33 +113,21 @@ except:
     print "Error accessing RHEV-M api, please check data and connection and retry"
     sys.exit(1)
 
-# Define VM based on parameters
-if __name__ == "__main__":
-    vmparams = params.VM(os=params.OperatingSystem(type_=options.osver),
-    cpu=params.CPU(topology=params.CpuTopology(cores=int(options.vmcpu))),
-    name=options.name, memory=1024 * 1024 * 1024 * int(options.vmmem),
-    cluster=api.clusters.get(name=options.cluster),
-    template=api.templates.get(name="Blank"), type_="server")
-    vmdisk = params.Disk(size=1024 * 1024 * 1024 * int(options.sdsize), wipe_after_delete=True, sparse=True, interface="virtio", type_="System", format="cow",
-    storage_domains=params.StorageDomains(storage_domain=[api.storagedomains.get(name="DATA1")]))
-    vmnet = params.NIC()
 
-    network_net = params.Network(name=options.vmnet)
-
-    nic_net1 = params.NIC(name='nic1', network=network_net, interface='virtio')
-
+# Define the function to add vms
+def add_vm(vmparams, name, vmdisk, nic_net1):
     try:
         api.vms.add(vmparams)
     except:
         print "Error creating VM with specified parameters, recheck"
         sys.exit(1)
 
-    if options.verbosity > 1:
+    if verbosity > 1:
         print "VM created successfuly"
 
-    if options.verbosity > 1:
+    if verbosity > 1:
         print "Attaching networks and boot order..."
-    vm = api.vms.get(name=options.name)
+    vm = api.vms.get(name=name)
     vm.nics.add(nic_net1)
 
     try:
@@ -98,26 +136,50 @@ if __name__ == "__main__":
         print "Error attaching networks, please recheck and remove configurations left behind"
         sys.exit(1)
 
-    if options.verbosity > 1:
+    if verbosity > 1:
         print "Adding HDD"
     try:
         vm.disks.add(vmdisk)
     except:
         print "Error attaching disk, please recheck and remove any leftover configuration"
+        sys.ext(1)
 
-    if options.verbosity > 1:
+    if verbosity > 1:
         print "VM creation successful"
 
-    vm = api.vms.get(name=options.name)
+    vm = api.vms.get(name=name)
     vm.high_availability.enabled = True
     vm.update()
 
-#wait until VM is stopped before we start it.
-status = api.vms.get(name=options.name).status.state
-while status != 'down':
-		print status
-		time.sleep(5)
-		status = api.vms.get(name=options.name).status.state
-		vm.start()
+    #wait until VM is stopped before we start it.
+    status = api.vms.get(name=name).status.state
+    while status != 'down':
+        print status
+	time.sleep(1)
+	status = api.vms.get(name=name).status.state
+	vm.start()
 
+# Define VM based on parameters
+if __name__ == "__main__":
+    vmparams = params.VM(os=params.OperatingSystem(type_=osver),
+            cpu=params.CPU(topology=params.CpuTopology(cores=int(vmcpu))),
+            name=name, 
+            memory=1024 * 1024 * 1024 * int(vmmem),
+            cluster=api.clusters.get(name=cluster),
+            template=api.templates.get(name="Blank"), type_="server")
+    
+    vmdisk = params.Disk(
+            size=1024 * 1024 * 1024 * int(sdsize), 
+            wipe_after_delete=True, 
+            sparse=True, 
+            interface="virtio", 
+            type_="System", 
+            format="cow",
+            storage_domains=params.StorageDomains(storage_domain=[api.storagedomains.get(name=storage_name)]))
+    
+    vmnet = params.NIC()
+    network_net = params.Network(name=vmnet)
+    nic_net1 = params.NIC(name='nic1', network=network_net, interface='virtio')
+
+    add_vm(vmparams, name, vmdisk, nic_net1)
 #print "MAC:%s" % vm.nics.get(name="eth0").mac.get_address()
