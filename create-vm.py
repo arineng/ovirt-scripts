5f359e7de7fe9e28d8e9c88368d19c04c2526cd4
Jacob Castello <jacob@arin.net>
2015-11-05 17:03:16 -0500

    not messing up overrides this time

--- create-vm.py
+++ create-vm.py
@@ -23,6 +23,7 @@ import os
 import time
 import json
 import getpass
+import ConfigParser
 
 from ovirtsdk.api import API
 from ovirtsdk.xml import params
@@ -38,81 +39,27 @@ osver can be: rhel_6x64, etc
 """
 
 # Option parsing
-p = optparse.OptionParser("rhev-vm-create.py [arguments]", description=description)
-p.add_option("-u", "--user", dest="username", help="Username to connect to ovirt-engine API", metavar="admin@internal", default="admin@internal")
-p.add_option("-w", "--password", dest="password", help="Password to use with username", metavar="admin", default="redhat")
-p.add_option("-s", "--server", dest="server", help="RHEV-M server address/hostname to contact", metavar="server", default="127.0.0.1")
-p.add_option('-v', "--verbosity", dest="verbosity", help="Show messages while running", metavar='[0-n]', default=0, type='int')
-p.add_option("-n", "--name", dest="name", help="VM name", metavar="name", default="name")
-p.add_option("-c", "--cluster", dest="cluster", help="VM cluster", metavar="cluster", default="Default")
-p.add_option("--vmcpu", dest="vmcpu", help="VM CPU", metavar="vmcpu", default="1")
-p.add_option("--vmmem", dest="vmmem", help="VM RAM in GB", metavar="vmmem", default="1")
-p.add_option("--sdtype", dest="sdtype", help="SD type", metavar="sdtype", default="Default")
-p.add_option("--sdsize", dest="sdsize", help="SD size in GB", metavar="sdsize", default="20")
-p.add_option("--osver", dest="osver", help="OS version", metavar="osver", default="rhel_6x64")
-p.add_option("--vmnet", dest="vmnet", help="Network to use", metavar="vmnet", default="rhevm")
-p.add_option("--config", dest="config", help="JSON config file")
-p.add_option("--storage", dest="storage_name", help="Name of the storage domain")
-p.add_option("--ca", dest="ca_file", help="Path to the ca file")
-p.add_option("--insecure", dest="insecure", help="Connect without validating the CA")
-(options, args) = p.parse_args()
-
-# Use the json file to update configs if present
-if options.config:
-    with open(options.config) as config_params:
-        locals().update(json.load(config_params))
-
-# Overide anything in the config file
-# on the command line
-if options.username:
-    username = options.username
-if options.server:
-    server = options.server
-if options.name:
-    name = options.name
-if options.cluster:
-    cluster = options.cluster
-if options.vmcpu:
-    vmcpu = options.vmcpu
-if options.vmem:
-    vmem = options.vmem
-if options.sdtype:
-    sdtype = options.sdtype
-if options.sdsize:
-    sdsize = options.sdsize
-if options.osver:
-    osver = options.osver
-if options.vmnet:
-    vmnet = options.vmnet
-if options.verbosity:
-    verbosity = options.verbosity
-if options.storage_name:
-    storage_name = options.storage_name
-if options.ca_file:
-    ca_file = options.ca_file
-if options.insecure:
-    insecure = options.insecure
-
-# Allow for interactive password auth
-if options.password:
-    password = options.password
-else:
-    password = getpass.getpass()
-
-
-baseurl = "https://%s" % (server)
-
-if options.insecure:
-    api = API(url=baseurl, username=username, password=password, insecure=True)
-else:
-    api = API(url=baseurl, username=username, password=password, ca_file=ca_file)
-
-try:
-    value = api.hosts.list()
-except:
-    print "Error accessing RHEV-M api, please check data and connection and retry"
-    sys.exit(1)
-
+def getParser(defaults):
+    p = optparse.OptionParser("rhev-vm-create.py [arguments]", description=description)
+    p.set_defaults(**defaults)
+    p.add_option("-u", "--user", dest="username", help="Username to connect to ovirt-engine API", metavar="admin@internal", default="admin@internal")
+    p.add_option("-w", "--password", dest="password", help="Password to use with username", metavar="admin", default="redhat")
+    p.add_option("-s", "--server", dest="server", help="RHEV-M server address/hostname to contact", metavar="server", default="127.0.0.1")
+    p.add_option('-v', "--verbosity", dest="verbosity", help="Show messages while running", metavar='[0-n]', default=0, type='int')
+    p.add_option("-n", "--name", dest="name", help="VM name", metavar="name", default="name")
+    p.add_option("-c", "--cluster", dest="cluster", help="VM cluster", metavar="cluster", default="Default")
+    p.add_option("--vmcpu", dest="vmcpu", help="VM CPU", metavar="vmcpu", default="1")
+    p.add_option("--vmmem", dest="vmmem", help="VM RAM in GB", metavar="vmmem", default="1")
+    p.add_option("--sdtype", dest="sdtype", help="SD type", metavar="sdtype", default="Default")
+    p.add_option("--sdsize", dest="sdsize", help="SD size in GB", metavar="sdsize", default="20")
+    p.add_option("--osver", dest="osver", help="OS version", metavar="osver", default="rhel_6x64")
+    p.add_option("--vmnet", dest="vmnet", help="Network to use", metavar="vmnet", default="rhevm")
+    p.add_option("--config", dest="config", help="JSON config file")
+    p.add_option("--storage", dest="storage_name", help="Name of the storage domain")
+    p.add_option("--ca", dest="ca_file", help="Path to the ca file")
+    p.add_option("--insecure", dest="insecure", help="Connect without validating the CA")
+    
+    return p
 
 # Define the function to add vms
 def add_vm(vmparams, name, vmdisk, nic_net1):
@@ -161,6 +108,35 @@ def add_vm(vmparams, name, vmdisk, nic_net1):
 
 # Define VM based on parameters
 if __name__ == "__main__":
+   
+    my_defaults = {'verbose': 1}
+    opts, args = getParser(my_defaults).parse_args()
+    if opts.config is not None:
+        configToLoad = opts.config
+    else:
+        configToLoad = None
+
+    if configToLoad is not None:
+        loadedConfig = json.load(open(configToLoad))
+        my_defalts.update(loadedConfig)
+        opts, args = getParser(my_defaults).parse_args()
+
+    username = options.username
+    server = options.server
+    name = options.name
+    cluster = options.cluster
+    vmcpu = options.vmcpu
+    vmem = options.vmmem
+    sdtype = options.sdtype
+    sdsize = options.sdsize
+    osver = options.osver
+    vmnet = options.vmnet
+    verbosity = options.verbosity
+    storage_name = options.storage_name
+    ca_file = options.ca_file
+    insecure = options.insecure
+
+    
     vmparams = params.VM(os=params.OperatingSystem(type_=osver),
             cpu=params.CPU(topology=params.CpuTopology(cores=int(vmcpu))),
             name=name, 
@@ -181,5 +157,19 @@ if __name__ == "__main__":
     network_net = params.Network(name=vmnet)
     nic_net1 = params.NIC(name='nic1', network=network_net, interface='virtio')
 
+   
+    baseurl = "https://%s" % (server)
+    
+    if options.insecure:
+        api = API(url=baseurl, username=username, password=password, insecure=True)
+    else:
+        api = API(url=baseurl, username=username, password=password, ca_file=ca_file)
+    
+    try:
+        value = api.hosts.list()
+    except:
+        print "Error accessing RHEV-M api, please check data and connection and retry"
+        sys.exit(1)
+    
     add_vm(vmparams, name, vmdisk, nic_net1)
 #print "MAC:%s" % vm.nics.get(name="eth0").mac.get_address()
